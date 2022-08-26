d43c7110c7e7e6b94346f363eb9ff2c7f6d1e890
aman_dwivedi <amandwivedi71124@gmail.com>
2022-08-26 12:18:08 +0530

    updated day-4 files

--- template.js
+++ template.js
@@ -20,6 +20,9 @@ function append_data(el){
 
     let btn1 = document.createElement("button")
     btn1.innerText = "Lets Get Started"
+    btn1.addEventListener("click",function(){
+        location.href="signup.html"
+    })
 
     let btn2  = document.createElement("button")
     btn2.innerText = "See their Story"
